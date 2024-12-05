de8b34bb4c1524415b2687529ecacd942df9c12f
Piotr Bahlaj <piotr.bahlaj@o2.pl>
2024-12-05 15:11:03 +0100

    Dashboard search bar code refactor

--- lib/features/dashboard/presentation/widgets/search_bar.dart
+++ lib/features/dashboard/presentation/widgets/search_bar.dart
@@ -1,5 +1,6 @@
 import 'package:charge_route/%20core/utilities/debouncer.dart';
 import 'package:charge_route/features/dashboard/presentation/bloc/dashboard_bloc.dart';
+import 'package:charge_route/features/dashboard/presentation/widgets/search_suggestions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -130,34 +131,12 @@ class DashboardSearchBar extends StatelessWidget {
                       });
                     }
                     if (state.suggestions.isNotEmpty && state.activeField == field) {
-                      return SizedBox(
-                        height: 200,
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: state.suggestions.length,
-                          itemBuilder: (context, index) {
-                            final suggestion = state.suggestions[index];
-                            return Column(
-                              children: [
-                                ListTile(
-                                  title: Text(suggestion.description),
-                                  onTap: () {
-                                    bloc.add(FetchPlaceDetailsEvent(suggestion.placeId, field));
-                                    controller.text = suggestion.description;
-                                    focusNode.unfocus();
-                                    bloc.add(const ClearSuggestionsEvent());
-                                  },
-                                ),
-                                Divider(
-                                  color: Theme.of(context).colorScheme.onSecondary,
-                                  thickness: 0.5,
-                                  endIndent: 15,
-                                  indent: 15,
-                                ),
-                              ],
-                            );
-                          },
-                        ),
+                      return SearchSuggestions(
+                        prediction: state.suggestions,
+                        controller: controller,
+                        field: field,
+                        focusNode: focusNode,
+                        suggestionsLength: state.suggestions.length,
                       );
                     } else if (state.errorMessage != null && state.activeField == field) {
                       return Text(state.errorMessage!);
